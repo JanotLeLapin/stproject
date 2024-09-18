c6184be420167950ce4a4acd2d722aee3a004b1c
Joseph DALY <joseph300905@gmail.com>
2024-09-18 17:59:16 +0200

    use fread instead of fgets

--- bmg/decode.c
+++ bmg/decode.c
@@ -3,8 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 1024
-
 long
 read_int(char *buf, char bytes)
 {
@@ -21,32 +19,76 @@ read_int(char *buf, char bytes)
 void
 decode(void *f)
 {
-  char buf[BUFFER_SIZE];
-  size_t i, j, k = 0;
-  unsigned char encoding;
-  unsigned short message_count, data_size, inf_block_size;
-  unsigned int file_size, section_count, inf_section_size, message_offset, message_data;
+  char *buf = malloc(32);
   unsigned long *messages;
+  size_t i, j, buf_size = 33;
+  unsigned char encoding, c;
+  unsigned short message_count, data_size, inf_block_size;
+  unsigned int file_size, section_count, inf_section_size, message_offset, message_data, dat_section_size;
 
-  fgets(buf, 33, f);
+  fread(buf, 1, 32, f);
   file_size = (unsigned int) read_int(buf + 8, 4);
   section_count = (unsigned int) read_int(buf + 12, 4);
   encoding = buf[16] - 1;
   printf("HEADER\nfile size: %d\nsections: %d\nencoding: %s\n", file_size, section_count, encodings[encoding]);
 
-  fgets(buf, 17, f);
+  fread(buf, 1, 16, f);
   inf_section_size = (unsigned int) read_int(buf + 4, 4);
   message_count = (unsigned short) read_int(buf + 8, 2);
   inf_block_size = (unsigned short) read_int(buf + 10, 2);
-  printf("INF\nsection size: %d\nmessages: %d\ndata size: %d\n", inf_section_size, message_count, inf_block_size);
-  messages = malloc(4 * message_count);
+  printf("INF1\nsection size: %d\nmessages: %d\ndata size: %d\n", inf_section_size, message_count, inf_block_size);
 
-  fgets(buf, message_count * inf_block_size + 1, f);
+  if (buf_size <= inf_section_size - 16) {
+    buf_size = inf_section_size - 16;
+    buf = realloc(buf, buf_size);
+  }
+  messages = malloc(8 * message_count);
+
+  fread(buf, 1, inf_section_size - 16, f);
   for (i = 0; i < message_count; i++) {
-    message_offset = read_int(buf + i * inf_block_size, 4);
+    message_offset = (unsigned int) read_int(buf + i * inf_block_size, 4);
     messages[i] = message_offset;
     printf("offset: %d\n", message_offset);
   }
 
+  fread(buf, 1, 8, f);
+  dat_section_size = (unsigned int) read_int(buf + 4, 4);
+  printf("DAT1\nsection size: %d\n", dat_section_size);
+
+  if (buf_size <= dat_section_size - 8) {
+    buf_size = dat_section_size - 8;
+    buf = realloc(buf, buf_size);
+  }
+
+  fread(buf, 1, dat_section_size - 8, f);
+  for (i = 0; i < message_count; i++) {
+    for (j = messages[i]; 0 != buf[j]; j += 2) {
+      c = (unsigned char) buf[j];
+
+      switch (c) {
+        case 0x0a:
+          printf("<br>");
+          break;
+        case 0xe0:
+          printf("à");
+          break;
+        case 0xe8:
+          printf("è");
+          break;
+        case 0xe9:
+          printf("é");
+          break;
+        case 0xea:
+          printf("ê");
+          break;
+        default:
+          putchar(buf[j]);
+          break;
+      }
+    }
+    putchar('\n');
+  }
+
   free(messages);
+  free(buf);
 }
