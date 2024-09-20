29b34e6ea17458f2f86ba5b8ba3441a912f1f28d
Joseph DALY <joseph300905@gmail.com>
2024-09-20 18:54:58 +0200

    add useful programs to shell

--- shell.nix
+++ shell.nix
@@ -1,6 +1,13 @@
 { nil
-, clang-tools  
+, clang-tools
+, valgrind
+, xxd
+, desmume
 , mkShell
 }: mkShell {
-  buildInputs = [ nil clang-tools ];
+  buildInputs = [
+    nil clang-tools
+    valgrind xxd
+    desmume
+  ];
 }
