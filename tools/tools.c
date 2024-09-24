62b95a031b76459cc54c965f4fc46fddb280baad
Joseph DALY <joseph300905@gmail.com>
2024-09-24 10:59:08 +0200

    decoding works

--- tools/tools.c
+++ tools/tools.c
@@ -1,12 +1,12 @@
 #include <stdint.h>
+#include <string.h>
 #include <stdio.h>
 
 #include "tools.h"
 
-int
-main(void)
+static int
+decode_bmg(FILE *in, FILE *out)
 {
-  FILE *in = stdin;
   enum BmgDecodeResult res;
   struct BmgFile bmg;
   size_t i;
@@ -43,13 +43,15 @@ main(void)
         err = "invalid fli entry size";
         break;
       default:
-        return 1;
+        break;
     }
     fprintf(stderr, "decoding failed: %s\n", err);
+    fclose(in);
+    fclose(out);
     return 1;
   }
 
-  printf("BMG: inf entries: %d, flw instructions: %d, flw labels: %d, fli entries: %d\n\n", bmg.inf_entry_count, bmg.flw_instruction_count, bmg.flw_label_count, bmg.fli_entry_count);
+  printf("BMG: inf entries: %d, flw instructions: %d, flw labels: %d, fli entries: %d\n", bmg.inf_entry_count, bmg.flw_instruction_count, bmg.flw_label_count, bmg.fli_entry_count);
 
   /*
   for (i = 0; i < 3; i++) {
@@ -71,28 +73,75 @@ main(void)
     inf_entry = bmg_get_inf_entry(&bmg, i);
     bmg_get_dat_entry(&bmg, inf_entry.index, dat_entry, 1024);
 
-    printf("%d %s\n\n\n", inf_entry.attributes, dat_entry);
+    fprintf(out, "%d %s\n\n\n", inf_entry.attributes, dat_entry);
   }
 
   for (i = 0; i < bmg.flw_instruction_count; i++) {
     flw_instruction = bmg_get_flw_instruction(&bmg, i);
-    printf("%ld ", (unsigned long) flw_instruction);
+    fprintf(out, "%ld\n", (unsigned long) flw_instruction);
   }
 
-  printf("\n\n");
-
+  fprintf(out, "\n");
   for (i = 0; i < bmg.flw_label_count; i++) {
     flw_label = bmg_get_flw_label(&bmg, i);
     flw_id = bmg_get_flw_id(&bmg, i);
-    printf("(%d, %d) ", flw_id, flw_label);
+    fprintf(out, "(%d, %d)\n", flw_id, flw_label);
   }
 
-  printf("\n\n");
-
+  fprintf(out, "\n");
   for (i = 0; i < bmg.fli_entry_count; i++) {
     fli_entry = bmg_get_fli_entry(&bmg, i);
-    printf("(%d, %d) ", fli_entry.id, fli_entry.index);
+    fprintf(out, "(%d, %d)\n", fli_entry.id, fli_entry.index);
   }
 
   bmg_free_file(&bmg);
+
+  fclose(in);
+  fclose(out);
+
+  return 0;
+}
+
+int
+main(int argc, char **argv)
+{
+  char encode;
+  FILE *in, *out;
+
+  if (argc < 5) {
+    fprintf(stderr, "not enough arguments\n");
+    return 1;
+  }
+
+  if (!strncmp("encode", argv[2], 6)) {
+    encode = 1;
+  } else if (!strncmp("decode", argv[2], 6)) {
+    encode = 0;
+  } else {
+    fprintf(stderr, "unknown operation '%s'\n", argv[2]);
+    return 1;
+  }
+
+  in = fopen(argv[3], "r");
+  if (!in) {
+    fprintf(stderr, "could not open '%s'\n", argv[3]);
+    return 1;
+  }
+
+  out = fopen(argv[4], "w");
+  if (!out) {
+    fprintf(stderr, "could not open '%s'\n", argv[4]);
+    fclose(in);
+    return 1;
+  }
+
+  if (!strncmp("bmg", argv[1], 3)) {
+    if (encode) {}
+    else {
+      return decode_bmg(in, out);
+    }
+  } else {
+    fprintf(stderr, "unknown format '%s'\n", argv[1]);
+    return 1;
+  }
 }
